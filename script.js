eb9840933fcad1c2b3892b45187d591dcd8b28f2
Bhushan <waghdharebhushan71@gmail.com>
2025-09-20 15:18:52 +0530

    updated the code for design

---
+++ script.js
@@ -0,0 +1,8 @@
+    const menuBtn = document.querySelector('.menu-btn');
+    const navbar = document.querySelector('.navbar ul');
+
+  menuBtn.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+  });
+ 
+l̥
